b6293235e232042dc641fd8c755c67cdfe9771a4
huangbq4gh <huangbq.01@gmail.com>
2014-03-06 22:21:46 -0500

    Initial Commit

---
+++ TipsCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipsCalculator
+//
+//  Created by Bingqing Huang on 3/6/14.
+//  Copyright (c) 2014 Bingqing Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipsCalculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TipsCalculator
+//
+//  Created by Bingqing Huang on 3/6/14.
+//  Copyright (c) 2014 Bingqing Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
